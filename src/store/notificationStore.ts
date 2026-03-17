0ae11b319a9c239b7efc678e47a38274ea49e2b9
Pepe Ortiz Roldán <99665744+madgIitch@users.noreply.github.com>
2026-03-17 03:25:35 +0100

    puesta a 0

--- src/store/notificationStore.ts
+++
@@ -1,9 +0,0 @@
-// src/store/notificationStore.ts
-import { create } from 'zustand';
-import messaging from '@react-native-firebase/messaging';
-
-interface NotificationStore {
-  fcmToken: string | null;
-  requestPermission: () => Promise<void>;
-  getToken: () => Promise<void>;
-}
