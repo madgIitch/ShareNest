379c1df20f31ec6d1f39bf11cd7f6566cb08bfba
Pepe Ortiz Roldán <99665744+madgIitch@users.noreply.github.com>
2026-03-25 13:30:42 +0100

    reinicio

--- jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  preset: "jest-expo",
-  testPathIgnorePatterns: ["/node_modules/", "/e2e/"],
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-};
--- jest.setup.ts
+++
@@ -1,9 +0,0 @@
-jest.mock("expo-constants", () => ({
-  expoConfig: {
-    extra: {
-      appEnv: "test",
-      supabaseUrl: "https://example.supabase.co",
-      supabaseAnonKey: "test-anon-key",
-    },
-  },
-}));
